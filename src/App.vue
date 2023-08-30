b8768a127711d512e8ecb55f70637da376f10c73
Aman-Fenixwork <aman@fenixwork.com>
2023-08-30 11:50:06 +0530

    How to use for loop and how to loop arrays and objects

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,12 @@ data() {
     arrayCondition2 : true,
     display : true,
     age : 18,
+    names : ["Aman","Krishna","Chinmay"],
+    ageGroup : [{
+      name: "Ram",age : 22},
+     { name : "Sham",age : 26, },
+      {name: "Golu",age:24},
+    ],
     flexStyles : {
       display : "flex", 
       "justify-content" :"center",
@@ -54,6 +60,12 @@ data() {
     <p v-else-if="age < 18">If age is less then 18</p>
     <p v-else>No age limit</p>
   </template>
+
+  <!-- For loop  -->
+
+  <h1 v-for="name in names" :key="name">{{ name }}</h1>
+  <h1 v-for="(name, index) in names" :key="name">{{index+1}} {{ name }}</h1>
+  <span v-for="(person,index) in ageGroup" :key="index">{{person.name}}{{ person.age }}</span>
 </template>
 
 <style scoped>
